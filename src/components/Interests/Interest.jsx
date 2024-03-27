525f41557619e9d60c17edc6fa2f3b55ec2d3b92
darrk-heart <prospaul999@gmail.com>
2024-03-27 04:33:35 +0000

    working

--- src/components/Interests/Interest.jsx
+++ src/components/Interests/Interest.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function Interest() {
   return (
-    <div>Interest</div>
-  )
+    <>
+      <div style={{ display: "flex" }}>
+        <h4
+          style={{
+            margin: "0",
+            padding: "0",
+            fontWeight: "400",
+            fontSize: "1.1rem",
+            marginRight: "30px",
+          }}
+        >
+          You may be interested in
+        </h4>{" "}
+        <span
+          style={{
+            border: "1px solid black",
+            fontSize: "0.7rem",
+            padding: "4px 8px 2px 8px",
+            borderRadius: "50%",
+          }}
+        >
+          i
+        </span>
+      </div>
+    </>
+  );
 }
 
-export default Interest
+export default Interest;
