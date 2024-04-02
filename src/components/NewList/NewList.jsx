1841c26c933ea82fd8a2467959175983cd1450ae
darrk-heart <prospaul999@gmail.com>
2024-04-02 12:16:38 +0000

    working

--- src/components/NewList/NewList.jsx
+++ src/components/NewList/NewList.jsx
@@ -3,16 +3,15 @@ import styles from "./NewList.module.css";
 
 function NewList({ onClose }) {
   const inputRef = useRef(null);
-
-  const handleCloseNewList = () => {
-    onClose();
-  };
-
   const [inputValue, setInputValue] = useState("");
   const [saveColor, setSaveColor] = useState("#666666");
   const [saveBgColor, setSaveBgColor] = useState("#eeeeee");
   const [divBorder, setDivBorder] = useState("#164de5");
   const [emptyText, setEmptyText] = useState("white");
+
+  const handleCloseNewList = () => {
+    onClose();
+  };
 
   const handleInputChange = (event) => {
     const value = event.target.value;
@@ -26,56 +25,59 @@ function NewList({ onClose }) {
   useEffect(() => {
     inputRef.current.focus();
   }, []);
+
   return (
-    <>
-      <div className={styles.overlay}>
-        <div className={styles.content}>
-          <h3>Create a new list</h3>
-          <div style={{ borderColor: divBorder }} className={styles.enter}>
-            <input
-              value={inputValue}
-              onChange={handleInputChange}
-              type="text"
-              ref={inputRef}
-              placeholder="New Item"
-            />
-          </div>
+    <div className={styles.overlay}>
+      <div className={styles.content}>
+        <h3>Create a new list</h3>
+        <div style={{ borderColor: divBorder }} className={styles.enter}>
+          <input
+            value={inputValue}
+            onChange={handleInputChange}
+            type="text"
+            ref={inputRef}
+            placeholder="New Item"
+          />
+        </div>
+        <span
+          style={{
+            color: emptyText,
+            fontSize: "0.8rem",
+            fontWeight: "400",
+            paddingLeft: "13px",
+            paddingTop: "10px",
+          }}
+        >
+          Enter a name
+        </span>
+        <div className={styles.cancelSave}>
           <span
+            className={styles.cancel}
             style={{
-              color: emptyText,
-              fontSize: "0.8rem",
-              fontWeight: "400",
-              paddingLeft: "10px",
-              paddingTop: "5px",
+              color: "#1a73e8",
+              padding: "10px",
+              borderRadius: "2px",
+            }}
+            onClick={handleCloseNewList}
+          >
+            Cancel
+          </span>
+          <span
+            className={styles.save}
+            style={{
+              color: saveColor,
+              padding: "10px",
+              marginRight: "5px",
+              marginLeft: "15px",
+              borderRadius: "2px",
+              backgroundColor: saveBgColor,
             }}
           >
-            Enter a name
+            Save
           </span>
-          <div className={styles.cancelSave}>
-            <span
-              className={styles.cancel}
-              style={{ color: "#1a73e8", padding: "10px", borderRadius: "2px" }}
-              onClick={handleCloseNewList}
-            >
-              Cancel
-            </span>
-            <span
-              className={styles.save}
-              style={{
-                color: saveColor,
-                padding: "10px",
-                marginRight: "5px",
-                marginLeft: "15px",
-                borderRadius: "2px",
-                backgroundColor: saveBgColor,
-              }}
-            >
-              Save
-            </span>
-          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
