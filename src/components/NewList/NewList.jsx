5231d202305a660c0964590cb714be4765868653
darrk-heart <prospaul999@gmail.com>
2024-04-02 09:21:22 +0000

    working

--- src/components/NewList/NewList.jsx
+++ src/components/NewList/NewList.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./NewList.module.css";
 
 function NewList({ onClose }) {
+  const inputRef = useRef(null);
+
   const handleCloseNewList = () => {
     onClose();
   };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <>
       <div className={styles.overlay}>
         <div className={styles.content}>
           <h3>Create a new list</h3>
-          <input />
+          <input type="text" ref={inputRef} />
           <div>
-            <span onClick={handleCloseNewList}>Cancel</span>
-            <span>Save</span>
+            <span style={{ color: "#1a73e8" }} onClick={handleCloseNewList}>
+              Cancel
+            </span>
+            <span style={{ color: "#1a73e8" }}>Save</span>
           </div>
         </div>
       </div>
