14366d01ba759e7fe55af42c3796ff36e74e1568
darrk-heart <prospaul999@gmail.com>
2024-04-04 04:22:05 +0000

    working

--- src/components/MostFollowed/MostFollowed.jsx
+++ src/components/MostFollowed/MostFollowed.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function MostFollowed() {
-  return <div>MostFollowed</div>;
+  const [appleData, setAppleData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.polygon.io/v3/reference/tickers/AAPL?apiKey=nb4AsP75pkAKlg_4iyruc8nyZW72hCgd"
+        );
+        const data = await response.json();
+        console.log(data);
+        setAppleData(data.results);
+      } catch (error) {
+        console.error("Error fetching Apple data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Apple (AAPL) Information</h2>
+      {appleData ? (
+        <div>
+          <p>Symbol: {appleData.ticker}</p>
+          <p>Name: {appleData.name}</p>
+          <p>Market Type: {appleData.market}</p>
+          <p>Currency: {appleData.currency_name}</p>
+          <p>Phone: {appleData.phone_number}</p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 }
 
 export default MostFollowed;
